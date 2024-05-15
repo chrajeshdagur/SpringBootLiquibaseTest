80eae84dd1dec1bf12c66a064916ed49700b0346
Rajesh Dagur <chrajeshdagur@gmail.com>
2024-05-15 09:06:48 +0530

    code upload

---
+++ src/main/java/com/expliqui3/demoliqui/DemoliquiApplication.java
@@ -0,0 +1,15 @@
+package com.expliqui3.demoliqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoliquiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Starting....");
+		SpringApplication.run(DemoliquiApplication.class, args);
+		System.out.println("Hello end....");
+	}
+
+}
---
+++ src/test/java/com/expliqui3/demoliqui/DemoliquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expliqui3.demoliqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoliquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
